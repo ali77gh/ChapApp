ca99b3b8a863cd6b2417a8cb4569c4d4df44b5ed
Ali ghahremani <alighahremani1377@gmail.com>
2023-10-09 15:35:00 +0330

    simple code editor and result show

--- webapp/src/main.rs
+++ webapp/src/main.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 // import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
-use dioxus::prelude::*;
+use dioxus::{prelude::*};
 
 fn main() {
     // launch the web app
@@ -9,9 +9,28 @@ fn main() {
 
 // create a component that renders a div with the text "Hello, world!"
 fn App(cx: Scope) -> Element {
+
+    let output = use_state(cx, || "".to_string());
+    let input = use_state(cx, || "".to_string());
+
     cx.render(rsx! {
+        textarea {
+            class: "left split",
+            oninput: move |evt| input.set(evt.value.clone()),
+            "{input}"
+        },
         div {
-            "Hello, world!"
+            class: "right split",
+            white_space: "pre-wrap", // make \n works
+            "{output}"
+        },
+        div {
+        
+            class: "centered fab",
+            onclick: move |_| {
+                output.set(input.get().clone());
+            },
+            "->"
         }
     })
 }
